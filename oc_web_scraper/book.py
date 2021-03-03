4ce4eb868ad51570198203f8e7aad7b34391ad33
PabloLec <pablo.lecolinet@gmail.com>
2021-03-03 14:15:28 +0100

    Added docstrings for Boook class

--- oc_web_scraper/book.py
+++ oc_web_scraper/book.py
@@ -1,15 +1,41 @@
 import requests
 import re
 
-from bs4 import BeautifulSoup
-from bs4 import element
+from bs4 import BeautifulSoup, element
 
 from oc_web_scraper import errors as _CUSTOM_ERRORS
 
 
 class Book:
-    def __init__(self, title: str, url: str, category: str):
+    """Book class manages book page scraping and
+    stores its unique product information.
 
+    Attributes:
+        image_relative_path (str): Relative path hard-coded in image URLs.
+        image_absolute_path (str): Absolute equivalent of the relative path.
+        title (str): Book title. Passed in instantiation arguments.
+        url (str): Book page URL. Passed in instantiation arguments.
+        category (str): Book category. Passed in instantiation arguments.
+        product_description (str): Product description, set during infos scraping.
+        upc (str): UPC, set during infos scraping.
+        price_including_tax (str): Price including tax, set during infos scraping.
+        price_excluding_tax (str): Price excluding tax, set during infos scraping.
+        number_available (str): Number of books available, set during infos scraping.
+        review_rating (str): Review rating, set during infos scraping.
+        image_url (str): Book cover image URL, set during infos scraping."""
+
+    def __init__(self, title: str, url: str, category: str):
+        """Constructor for Book class.
+
+        Args:
+            title (str): Book title
+            url (str): URL of book page
+            category (str): Category of the book
+        """
+
+        # Image URL relative part and its absolute equivalent
+        # are hard coded to ease eventual adaptation for
+        # future website structure modifications.
         self.image_relative_path = "../../"
         self.image_absolute_path = "https://books.toscrape.com"
 
@@ -22,15 +48,12 @@ class Book:
         self.price_including_tax = None
         self.price_excluding_tax = None
         self.number_available = None
-        self.product_description = None
         self.review_rating = None
         self.image_url = None
 
         self.scrap_book()
-        print(self)
 
     def __str__(self):
-
         print_content = " - Title: {title}\n".format(title=self.title)
         print_content += "  - URL: {url}\n\n".format(url=self.url)
         print_content += "  - Description: {description}\n\n".format(
@@ -54,6 +77,19 @@ class Book:
         return print_content
 
     def scrap_book(self):
+        """Scraping process for book pages.
+        Relevant infos are picked using bs4 and stored in class
+        attributes.
+
+        Raises:
+            _CUSTOM_ERRORS.NoImageFound: If book cover is not found in page.
+            _CUSTOM_ERRORS.NoRatingFound: If book rating is not found in page.
+            _CUSTOM_ERRORS.NoProductDescriptionFound: If book description is
+            not found in page.
+            _CUSTOM_ERRORS.NoProductInformationFound: If book information
+            container is not found in page.
+        """
+
         raw_response = requests.get(self.url)
         soup = BeautifulSoup(raw_response.content, "html.parser")
 
@@ -99,7 +135,18 @@ class Book:
 
         self.parse_product_info(raw_product_information)
 
-    def set_rating(self, raw_rating: str):
+    def set_rating(self, raw_rating: element.Tag):
+        """Converts string literal rating values to int value
+        from the raw rating DOM element class attributes.
+
+        Args:
+            raw_rating (element.Tag): Raw DOM element scrapped beforehand.
+
+        Raises:
+            _CUSTOM_ERRORS.FailedToGetRating: If class attributes literal does not
+            match any rating from 0 to 5.
+        """
+
         class_attributes = raw_rating["class"]
 
         if "Zero" in class_attributes:
@@ -118,6 +165,17 @@ class Book:
             raise _CUSTOM_ERRORS.FailedToGetRating(class_attributes)
 
     def parse_product_info(self, raw_info: element.Tag):
+        """Parses informations displayed within the information
+        container and set class attributes values.
+
+        Args:
+            raw_info (element.Tag): Raw DOM element scrapped beforehand.
+
+        Raises:
+            _CUSTOM_ERRORS.BookInfoParsingFailed: If at least one of the
+            searched informations is missing.
+        """
+
         info_lines = raw_info.find_all("tr")
 
         for line in info_lines:
@@ -132,6 +190,7 @@ class Book:
             elif "Availability" in line_text:
                 self.set_number_available(raw_line=line)
 
+        # After parsing, verify if all infos were found.
         mandatory_attr_not_set = [
             self.upc is None,
             self.price_including_tax is None,
@@ -143,6 +202,16 @@ class Book:
             raise _CUSTOM_ERRORS.BookInfoParsingFailed(title=self.title)
 
     def set_upc(self, raw_line: element.Tag):
+        """Sets upc class attribute from raw DOM element.
+
+        Args:
+            raw_line (element.Tag): Raw DOM element.
+
+        Raises:
+            _CUSTOM_ERRORS.CouldNotParseInfo: If the info cannot be found.
+            Missing info will be mentionned in error message.
+        """
+
         value = raw_line.find("td")
 
         if value is None:
@@ -151,6 +220,16 @@ class Book:
         self.upc = value.get_text().strip()
 
     def set_price_including_tax(self, raw_line: element.Tag):
+        """Sets price_including_tax class attribute from raw DOM element.
+
+        Args:
+            raw_line (element.Tag): Raw DOM element.
+
+        Raises:
+            _CUSTOM_ERRORS.CouldNotParseInfo: If the info cannot be found.
+            Missing info will be mentionned in error message.
+        """
+
         value = raw_line.find("td")
 
         if value is None:
@@ -161,6 +240,16 @@ class Book:
         self.price_including_tax = value.get_text().strip()
 
     def set_price_excluding_tax(self, raw_line: element.Tag):
+        """Sets price_excluding_tax class attribute from raw DOM element.
+
+        Args:
+            raw_line (element.Tag): Raw DOM element.
+
+        Raises:
+            _CUSTOM_ERRORS.CouldNotParseInfo: If the info cannot be found.
+            Missing info will be mentionned in error message.
+        """
+
         value = raw_line.find("td")
 
         if value is None:
@@ -171,6 +260,16 @@ class Book:
         self.price_excluding_tax = value.get_text().strip()
 
     def set_number_available(self, raw_line: element.Tag):
+        """Sets number_available class attribute from raw DOM element.
+
+        Args:
+            raw_line (element.Tag): Raw DOM element.
+
+        Raises:
+            _CUSTOM_ERRORS.CouldNotParseInfo: If the info cannot be found.
+            Missing info will be mentionned in error message.
+        """
+
         value = raw_line.find("td")
 
         if value is None:
