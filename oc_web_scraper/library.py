4927ba539416581cd3fd0df8ffd5ade2ffb8060d
PabloLec <pablo.lecolinet@gmail.com>
2021-03-05 17:38:25 +0100

    Added docstrings for Library

--- oc_web_scraper/library.py
+++ oc_web_scraper/library.py
@@ -2,10 +2,24 @@ from oc_web_scraper.category import Category
 
 
 class Library:
+    """Library class handles scrapping content collection and storage
+    by instantiating Category objects."""
+
     def __init__(self):
+        """Constructor class for Library class.
+
+        Attributes:
+            categories (dict): Categories scrapped in the main website page.
+        """
 
         self.categories = {}
 
     def create_category(self, name: str, url: str):
+        """Instantiate a Category object with scrapped infos
+
+        Args:
+            name (str): Name of the category.
+            url (str): URL of the category page.
+        """
         category_object = Category(name=name, url=url)
         self.categories[name] = category_object
